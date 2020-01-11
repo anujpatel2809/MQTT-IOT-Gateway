07cefbf6e24cc7c3b62750ec31a3e43fb133fe09
Anuj Dekavadiya <anujsureshbhai.dekavadiya@team.telstra.com>
2020-01-11 22:56:19 +0530

    refactored code

--- src/main/java/com/makethone/outliers/IotLocalGatewayApplication.java
+++ src/main/java/com/makethone/outliers/IotLocalGatewayApplication.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -23,61 +21,60 @@ import java.util.Map;
 @SpringBootApplication
 public class IotLocalGatewayApplication {
 
-	static Map<String,DeviceData> deviceCache = new HashMap<>();
+    static Map<String, DeviceData> deviceCache = new HashMap<>();
 
-	public static void main(String[] args) throws MqttException {
-		ConfigurableApplicationContext context= SpringApplication.run(IotLocalGatewayApplication.class, args);
+    public static void main(String[] args) throws MqttException {
+        ConfigurableApplicationContext context = SpringApplication.run(IotLocalGatewayApplication.class, args);
 
-		IMqttClient mqttLocalClient = (IMqttClient) context.getBean("mqttLocalClient");
-		IMqttClient mqttTBClient = (IMqttClient) context.getBean("mqttTBClient");
-		RestTemplate restTemplate = context.getBean(RestTemplate.class);
-		System.out.println(context.getEnvironment().getProperty("hr-ms.url"));
+        IMqttClient mqttLocalClient = (IMqttClient) context.getBean("mqttLocalClient");
+        IMqttClient mqttTBClient = (IMqttClient) context.getBean("mqttTBClient");
+        RestTemplate restTemplate = context.getBean(RestTemplate.class);
 
-		ObjectMapper objectMapper=new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-		mqttLocalClient.subscribe("v1/devices/me/telemetry",(topic,msg)->{
-			System.out.println(msg.getId() + " -> " + new String(msg.getPayload()));
+        mqttLocalClient.subscribe("v1/devices/me/telemetry", (topic, msg) -> {
+            System.out.println("Received -> " + new String(msg.getPayload()));
 
-			SensorData sensorData = objectMapper.readValue(msg.getPayload(), SensorData.class);
+            SensorData sensorData = objectMapper.readValue(msg.getPayload(), SensorData.class);
 
-			if(deviceCache.containsKey(sensorData.getDeviceId())) {
-				if(deviceCache.get(sensorData.getDeviceId()).getAuthorized()==true) {
-					MqttMessage mqttMessage = new MqttMessage();
-					mqttMessage.setPayload(msg.getPayload());
-					mqttMessage.setQos(2);
-					mqttMessage.setRetained(true);
-					mqttTBClient.publish("v1/devices/me/telemetry", mqttMessage);
-				}
-			}else{
-				deviceCache.put(sensorData.getDeviceId(),new DeviceData(sensorData.getDeviceId(),null,false));
+            if (deviceCache.containsKey(sensorData.getDeviceId())) {
+                if (deviceCache.get(sensorData.getDeviceId()).getAuthorized() == true) {
+                    MqttMessage mqttMessage = new MqttMessage();
+                    mqttMessage.setPayload(msg.getPayload());
+                    mqttMessage.setQos(2);
+                    mqttMessage.setRetained(true);
+                    mqttTBClient.publish("v1/devices/me/telemetry", mqttMessage);
+                }
+            } else {
+                deviceCache.put(sensorData.getDeviceId(), new DeviceData(sensorData.getDeviceId(), null, false));
 
-				HttpHeaders httpHeaders=new HttpHeaders();
-				httpHeaders.set("source","GATEWAY");
-				HttpEntity httpEntity=new HttpEntity(httpHeaders);
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.set("source", "GATEWAY");
+                HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
-				try {
-					ResponseEntity<DeviceData> responseEntity = restTemplate.exchange(context.getEnvironment().getProperty("hr-ms.url")+sensorData.getDeviceId(), HttpMethod.GET, httpEntity, DeviceData.class);
-					DeviceData deviceData=new DeviceData(sensorData.getDeviceId(),responseEntity.getBody().getAccessToken(),true);
-					deviceCache.put(sensorData.getDeviceId(),deviceData);
+                try {
+                    ResponseEntity<DeviceData> responseEntity = restTemplate.exchange(context.getEnvironment().getProperty("hr-ms.url") + sensorData.getDeviceId(), HttpMethod.GET, httpEntity, DeviceData.class);
+                    DeviceData deviceData = new DeviceData(sensorData.getDeviceId(), responseEntity.getBody().getAccessToken(), true);
+                    deviceCache.put(sensorData.getDeviceId(), deviceData);
 
-					MqttMessage mqttMessage = new MqttMessage();
-					mqttMessage.setPayload(objectMapper.writeValueAsString(sensorData).getBytes());
-					mqttMessage.setQos(2);
-					mqttMessage.setRetained(true);
-					mqttTBClient.publish("v1/devices/me/telemetry", mqttMessage);
+                    MqttMessage mqttMessage = new MqttMessage();
+                    mqttMessage.setPayload(objectMapper.writeValueAsString(sensorData).getBytes());
+                    mqttMessage.setQos(2);
+                    mqttMessage.setRetained(true);
+                    mqttTBClient.publish("v1/devices/me/telemetry", mqttMessage);
 
-				}catch (HttpStatusCodeException e){
-					DeviceData deviceData=new DeviceData(sensorData.getDeviceId(),null,false);
-					deviceCache.put(sensorData.getDeviceId(),deviceData);
-				}
-			}
-		});
+                } catch (HttpStatusCodeException e) {
+                    DeviceData deviceData = new DeviceData(sensorData.getDeviceId(), null, false);
+                    deviceCache.put(sensorData.getDeviceId(), deviceData);
+                }
+            }
+        });
 
-	}
+    }
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
